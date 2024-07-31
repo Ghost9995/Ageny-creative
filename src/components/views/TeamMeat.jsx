a8700a69b2b3b6b970d4312282742c888d4a3fc1
Faxri Hasumov <f4880618@gmail.com>
2024-07-31 16:01:49 +0400

    asdasd

---
+++ src/components/views/TeamMeat.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '../ui/Button';
+import image2 from "../../assets/img/image2.png";
+import { FaStar } from "react-icons/fa";
+
+function TeamMeat() {
+  return (
+    <>
+    <div className='flex m-0 auto'>
+        <div className="flex flex-col ">
+        </div>
+    </div>
+     </>
+  )
+}
+
+export default TeamMeat
